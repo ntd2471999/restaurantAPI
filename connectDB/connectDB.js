9d57cb0f19b022d528c6a451ac3fc1129a0fcd84
ntd2471999 <nguyenthanhduy247199@gmail.com>
2019-07-20 12:03:30 +0700

    make it better

--- connectDB/connectDB.js
+++ connectDB/connectDB.js
@@ -1,10 +1,10 @@
 const { Pool, Client } = require('pg');
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'RestaurantDB',
-    password: '123',
+    user: 'zwjznreyjddruj',
+    host: 'ec2-107-20-173-2.compute-1.amazonaws.com',
+    database: 'd8m54tgh9306o5',
+    password: '9f800949fd17f794ce43cc41a3eb8d718d18cfe0fdb4a5b91524e2edbdb36cdc',
     port: 5432,
   })
 
