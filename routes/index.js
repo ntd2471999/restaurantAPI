42243ba305c4bc814d3918095bdc883e736bc9cc
ntd2471999 <nguyenthanhduy247199@gmail.com>
2019-07-20 12:06:18 +0700

    make it better

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   
-  res.render('index',{title : "Index"});
+  res.render('index',{title : "Smart Restaurant API"});
 });
 
 module.exports = router;
